d78d30eb8c8515a12e74ce49a1028c6f821a886e
grbalmeida <g.r.almeida@live.com>
2023-03-26 23:20:14 -0300

    Reaproveitando o Menu

--- aluroni/src/pages/Inicio/index.tsx
+++ aluroni/src/pages/Inicio/index.tsx
@@ -1,3 +1,4 @@
+import Menu from 'components/Menu';
 import cardapio from 'data/cardapio.json';
 import styles from './Inicio.module.scss';
 
@@ -7,6 +8,7 @@ export default function Inicio() {
 
   return (
     <section>
+      <Menu />
       <h3 className={styles.titulo}>
         Recomendações da cozinha
       </h3>
