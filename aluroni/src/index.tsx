6a3afad556278ad212c68d0b1db6044379b6eb2e
grbalmeida <g.r.almeida@live.com>
2023-03-26 23:11:23 -0300

    Usando react-router-dom

--- aluroni/src/index.tsx
+++ aluroni/src/index.tsx
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
-import Inicio from 'pages/Inicio';
-import Cardapio from 'pages/Cardapio';
-
-const componenteAtual = window.location.pathname === '/' ? <Inicio /> : <Cardapio />;
+import Router from './routes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    {componenteAtual}
+    <Router />
   </React.StrictMode>
 );
