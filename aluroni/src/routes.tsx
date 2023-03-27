f317895a51d575181af4987484a7ffabdc6ce7d5
grbalmeida <g.r.almeida@live.com>
2023-03-27 10:39:18 -0300

    Criando o NotFound

--- aluroni/src/routes.tsx
+++ aluroni/src/routes.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Inicio from 'pages/Inicio';
 import Cardapio from 'pages/Cardapio';
 import Sobre from 'pages/Sobre';
+import NotFound from 'pages/NotFound';
 import Menu from 'components/Menu';
+import Footer from 'components/Footer';
 import PaginaPadrao from 'components/PaginaPadrao';
 
 export default function AppRouter() {
   return (
-    <main>
+    <main className='container'>
       <Router>
         <Menu />
         <Routes>
@@ -15,8 +17,10 @@ export default function AppRouter() {
             <Route index element={<Inicio />} />
             <Route path='cardapio' element={<Cardapio />} />
             <Route path='sobre' element={<Sobre />} />
+            <Route path='*' element={<NotFound />} />
           </Route>
         </Routes>
+        <Footer />
       </Router>
     </main>
   );
