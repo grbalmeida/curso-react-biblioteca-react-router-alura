af3fef5c2b22b7daaa8d0e0a727588b9a1d364be
grbalmeida <g.r.almeida@live.com>
2023-03-27 15:54:59 -0300

    Usando o useParams

--- aluroni/src/pages/Prato/index.tsx
+++ aluroni/src/pages/Prato/index.tsx
@@ -1,16 +1,24 @@
-import { useLocation, useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import classNames from 'classnames';
 import styles from './Prato.module.scss';
 import cardapio from 'data/cardapio.json';
 
 export default function Prato() {
 
-  const { state } = useLocation();
-  const { prato } = state as { prato: typeof cardapio[0] };
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const prato = cardapio.find(item => item.id === Number(id));
+
+  if (!prato) {
+    return <></>;
+  }
 
   return (
     <>
-      <button className={styles.voltar}>
+      <button
+        className={styles.voltar}
+        onClick={() => navigate(-1)}
+      >
         {'< Voltar'}
       </button>
       <section className={styles.container}>
